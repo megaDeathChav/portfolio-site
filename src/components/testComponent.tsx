9361bccc4b58419d5a06189e5d30dfeb35ee71c6
Ana <007579995@coyote.csusb.edu>
2024-10-04 14:02:13 -0700

    Add counter functionality

---
+++ src/components/testComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function TestComponent() {
+  return (
+    <section>
+      <h2>Skills</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+    </section>
+  );
+}
