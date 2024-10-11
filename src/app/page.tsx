e0f7ccb511fc4fd644da407c501d13fc5765fad7
Ana <007579995@coyote.csusb.edu>
2024-10-11 14:38:32 -0700

    Base template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,61 @@
-'use client';
 // import Image from "next/image";
-// import TestComponent from "@/components/testComponent";
-import React, {useState, useEffect } from 'react'
-import TestComponent from '@/components/testComponent';
+
+// import Image from "next/image";
 
 export default function Home() {
-
-  //count name of variable, set count lets you change the state if the variable
-  //use state is a method
-  //use state makes those things (i.e setCounter)
-  const [counter, setCounter] = useState(0);
-
   return (
-    <body>
-      <button onClick={()=> {setCounter(counter+1)}}>Click me!</button>
-      <h1>Counter: {counter} </h1>
-    
+    <body className='h-screen bg-gray-100'>
+      <header className='flex justify-center bg-blue-600 p-4'>
+        <h1 className='px-12 text-white text-2xl font-bold'>My Awesome Website</h1>
+      </header>
+
+      <nav className='bg-blue-300 p-2'>
+        <ul className='flex text-white gap-x-4 ml-8'>
+          <li>
+            <a href='#' className='hover:text-blue-600'>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href='#' className='hover:text-blue-600'>
+              About
+            </a>
+          </li>
+          <li>
+            <a href='#' className='hover:text-blue-600'>
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <main className='container mx-auto mt-8 p-4'>
+        <section className='shadow-2xl rounded-xl p-6 mb-4'>
+          <h2 className='text-xl pb-6 font-semibold'>
+            Welcome to Our Site
+          </h2>
+          <p className=''>
+            This is a simple page to demonstrate Tailwind CSS classNamees. You&apos;ll be recreating this design using utility classNamees!
+          </p>
+        </section>
+
+        <section className='shadow-2xl rounded-xl p-6 mb-4'>
+          <h2 className='text-xl pb-6 font-semibold'>
+            Features
+          </h2>
+          <ul className='list-disc list-inside '>
+            <li>Responsive design</li>
+            <li>Beautiful typography</li>
+            <li>Easy to customize</li>
+          </ul>
+        </section>
+      </main>
+
+      <footer className='fixed bottom-0 bg-black w-screen py-4'>
+        <p className="text-white text-center">
+          &copy; 2024 My Awesome Website. All rights reserved.
+        </p>
+      </footer>
     </body>
   );
 }
-
-
-//React hooks:
-//useState
-//useEffect
-//
